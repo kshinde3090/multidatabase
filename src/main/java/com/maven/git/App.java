6a313d1477fbee95547fcc6ad9a2d2262e76a2c4
kshinde3090 <kshinde3090@gmail.com>
2018-12-05 00:05:57 -0800

    added by me

--- src/main/java/com/maven/git/App.java
+++ src/main/java/com/maven/git/App.java
@@ -8,9 +8,10 @@ public class App
 {
     public static void main( String[] args )
     {
-    	//kishor
+    
         System.out.println( "first git app" );
         // comment added by another developer
         System.out.println( "added by other developer" );
+        System.out.println("added by me");
     }
 }
