b2f899ae4c45cc9dd4138bde41dbd67e69841703
kshinde3090 <kshinde3090@gmail.com>
2018-12-05 00:04:05 -0800

    Update App.java

--- src/main/java/com/maven/git/App.java
+++ src/main/java/com/maven/git/App.java
@@ -9,7 +9,7 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "first git app" );
-        
+        // comment added by another developer
         System.out.println( "added by other developer" );
     }
 }
