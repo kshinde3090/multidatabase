9c112712f06bdf6f92e4e9a288f613e49b7ab79a
kshinde3090 <kshinde3090@gmail.com>
2018-12-04 23:54:55 -0800

    added by other developer

--- src/main/java/com/maven/git/App.java
+++ src/main/java/com/maven/git/App.java
@@ -9,5 +9,7 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "first git app" );
+        
+        System.out.println( "added by other developer" );
     }
 }
